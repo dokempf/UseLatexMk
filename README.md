8935c76ed30729516465f645a00f30adf3793aba
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2017-02-20 23:09:27 +0100

    Initial commit for UseLatexMk

---
+++ README.md
@@ -0,0 +1,47 @@
+# What is UseLatexMk?
+
+UseLatexMk is a cmake tool to build Latex documents from cmake projects.
+
+Instead of doing the tedious work of implementing all the logic on how to
+build Latex files in the CMake language (like the better known UseLATEX.cmake
+does), it uses the LatexMk tool found on the system. As a result, there are
+some advantages of using UseLatexMk.cmake over using UseLATEX.cmake:
+* Additional input files, like e.g. code listing, do not have to be
+  manually listed anymore. Dependency tracking is done by LatexMk instead
+  of cmake.
+* The generation procedure is more robust, because
+  it relies on an upstream tool with a wider userbase.
+
+There are currently some limitations to UseLatexMK:
+* We intentionally restrict ourselves to building PDFs, as we believe
+  that this covers 99% of todays use cases.
+
+# What is needed to use UseLatexMk from my project?
+
+You just need to
+* copy all the CMake modules (`*.cmake`) from this project into your project
+* make sure the path, where they are located, is in `CMAKE_MODULE_PATH`
+* Include them with `include(UseLatexMk)`
+
+Now, you are ready to use it!
+
+# How do I use it?
+
+Just use the following CMake function from anywhere in your project:
+
+```
+add_latex_document(SOURCE texsource
+                   [TARGET target]
+                   [REQUIRED]
+                   )
+```
+
+The arguments:
+* `SOURCE`: Required argument with a single tex source that defines the document to be built
+* `TARGET`: An optional target name, defaults to a suitable mangling of the given source and its path.
+* `REQUIRED`: Set this option to issue a fatal error if the document could not
+  be built. By default it is only skipped.
+
+# License
+
+This software is provided under a BSD license, you can find it in the `.cmake` files of the project.
---
+++ UseLatexMk.cmake
@@ -0,0 +1,89 @@
+# UseLatexMk.cmake is a CMake module to build Latex documents
+# from CMake.
+#
+# To use this project, just copy the cmake modules (*.cmake) from this
+# project into your project and use the functions documented below.
+# Please also make sure to maintain below license agreement when copying.
+#
+# For any further informations visit https://github.com/dokempf/UseLatexMk
+#
+# For usability instructions see README.md.
+#
+# Copyright (c) 2017, Dominic Kempf
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+#   list of conditions and the following disclaimer in the documentation and/or
+#   other materials provided with the distribution.
+#
+# * Neither the name of the Universität Heidelberg nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# Find LATEX and LatexMk
+find_package(LATEX)
+find_package(LatexMk)
+
+function(add_latex_document)
+  # Parse the input parameters to the function
+  set(OPTION REQUIRED)
+  set(SINGLE SOURCE TARGET)
+  set(MULTI)
+  include(CMakeParseArguments)
+  cmake_parse_arguments(LMK "${OPTION}" "${SINGLE}" "${MULTI}" ${ARGN})
+
+  if(LMK_UNPARSED_ARGUMENTS)
+    message("add_latex_document: Unparsed arguments! This often indicates typos in named arguments.")
+  endif()
+
+  # Apply default arguments and check for required arguments
+  if(NOT LMK_SOURCE)
+    message(FATAL_ERROR "No tex source specified for add_latex_document!")
+  endif()
+  if(NOT LMK_TARGET)
+    # Construct a nice target name from the source file
+    get_filename_component(LMK_TARGET ${LMK_SOURCE} ABSOLUTE)
+    file(RELATIVE_PATH LMK_TARGET ${CMAKE_SOURCE_DIR} ${LMK_TARGET})
+#     get_filename_component(LMK_TARGET ${LMK_TARGET} DIRECTORY)
+    string(REPLACE "/" "_" LMK_TARGET ${LMK_TARGET})
+    string(REPLACE "." "_" LMK_TARGET ${LMK_TARGET})
+  endif()
+
+  # Check the existence of the latexmk executable and skip/fail if not present
+  if(NOT (LATEXMK_FOUND AND PDFLATEX_COMPILER))
+    if(LMK_REQUIRED)
+      message(FATAL_ERROR "Some Latex documents were required by the project, but LATEX or LatexMk were not found!")
+    else()
+      return()
+    endif()
+  endif()
+
+  # Call the latexmk executable
+  add_custom_target(${LMK_TARGET}
+                    COMMAND ${LATEXMK_EXECUTABLE}
+                            -pdflatex=\"${PDFLATEX_COMPILER} -shell-escape -interaction=nonstopmode %O %S\"
+                            -pdf
+                            -outdir=\"${CMAKE_CURRENT_BINARY_DIR}\"
+                            ${LMK_SOURCE}
+                    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+                    )
+endfunction()
